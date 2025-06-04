450af6f9dd74b748154ea00c881fcee98d15e0ea
DylanDoty <127159556+DylanDoty@users.noreply.github.com>
2025-06-04 17:52:24 -0600

    Created Procupine enemy and added a line of code to reduce enemies left in EnemyMain

--- Assets/Scripts/Enemies/EnemyMain.cs
+++ Assets/Scripts/Enemies/EnemyMain.cs
@@ -132,6 +132,7 @@ public class EnemyMain : MonoBehaviour, IEnemy, IDamagable
 	internal virtual void Die()
 	{
 		gameObject.SetActive(false);
+		SpawnerManager.Instance.waves[SpawnerManager.Instance.currentWaveIndex].enemiesLeft--;
 	}
 
 	IEnumerator DamageOverTime(float damagePerTick, int numOfTicks, float duration)
