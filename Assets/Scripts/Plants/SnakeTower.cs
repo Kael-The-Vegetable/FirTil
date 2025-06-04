b990b0d128db45694eaad8f2bed827ef33c68770
Malachi Munro <98977314+Bluffpenguin@users.noreply.github.com>
2025-06-04 17:52:26 -0600

    Working on vfx

--- Assets/Scripts/Plants/SnakeTower.cs
+++ Assets/Scripts/Plants/SnakeTower.cs
@@ -7,12 +7,9 @@ public class SnakeTower : PlantMain
 	[SerializeField] GameObject bulletPrefab;
 	[SerializeField] GameObject target;
 
-	private Animator animator;
-
 	public override void Awake()
 	{
 		base.Awake();
-		animator = GetComponentInChildren<Animator>();
 	}
 	public override void Update()
 	{
@@ -33,7 +30,7 @@ public class SnakeTower : PlantMain
 	}
 	public override void Activate()
 	{
-		animator.SetTrigger("Attack");
+		bodyAnim.SetTrigger("Attack");
 		Vector3 direction = target.transform.position - transform.position;
 		float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 		GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
@@ -44,8 +41,8 @@ public class SnakeTower : PlantMain
 	void AnimateLook()
 	{
 		Vector3 animDirection = (target.transform.position - transform.position).normalized;
-		animator.SetFloat("LookX", animDirection.x);
-		animator.SetFloat("LookY", animDirection.y);
+		bodyAnim.SetFloat("LookX", animDirection.x);
+		bodyAnim.SetFloat("LookY", animDirection.y);
 		if (animDirection.x < 0)
 		{
 			bodySprite.flipX = true;
--- Assets/Scripts/Plants/WaterPlant.cs
+++ Assets/Scripts/Plants/WaterPlant.cs
@@ -4,6 +4,9 @@ public class WaterPlant : PlantMain
 {
 	[SerializeField] LayerMask allyMask;
 
+	[Header("Water Effect")]
+	[SerializeField] private Animator waterEffect;
+
 	public override void Update()
 	{
 		base.Update();
@@ -15,6 +18,7 @@ public class WaterPlant : PlantMain
 	}
 	public override void Activate()
 	{
+		waterEffect.SetTrigger("Burst");
 		Collider2D[] hitAllies = Physics2D.OverlapCircleAll(transform.position, currentRange, allyMask);
 
 		if (hitAllies.Length <= 0) return;
