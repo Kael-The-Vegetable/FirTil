8eb240f15306df61197e173689c4434aac23847a
Malachi Munro <98977314+Bluffpenguin@users.noreply.github.com>
2025-06-04 19:11:52 -0600

    Added SeedInventory

---
+++ Assets/Scripts/Plants/SeedInventory.cs
@@ -0,0 +1,100 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SeedItem
+{
+    public Seed seed;
+    public int quantity;
+
+    public SeedItem(Seed seed, int quantity)
+	{
+		this.seed = seed;
+		this.quantity = quantity;
+	}
+}
+
+public class SeedInventory : MonoBehaviour
+{
+    public List<SeedItem> seeds = new();
+	public int equippedSeed = 0;
+
+	public void AddSeed(Seed seed, int amount)
+	{
+		SeedItem existingItem = seeds.Find(i => i.seed == seed);
+
+		if (existingItem != null && existingItem.quantity < existingItem.seed.MaxStackSize)
+		{
+			existingItem.quantity += amount;
+		}
+		else
+		{
+			seeds.Add(new SeedItem(seed, amount));
+		}
+	}
+
+	public void RemoveItem(Seed seed, int amount)
+	{
+		SeedItem existingItem = seeds.Find(i => i.seed == seed);
+
+		if (existingItem != null)
+		{
+			existingItem.quantity -= amount;
+			if (existingItem.quantity <= 0)
+			{
+				seeds.Remove(existingItem);
+			}
+		}
+		CheckEquippedSeed();
+	}
+
+	public void NextSeed()
+	{
+		if (equippedSeed + 1 == seeds.Count)
+		{
+			equippedSeed = 0;
+		}
+		else equippedSeed += 1;
+	}
+
+	public void PreviousSeed()
+	{
+		if (equippedSeed == 0)
+		{
+			equippedSeed = seeds.Count - 1;
+		}
+		else equippedSeed -= 1;
+	}
+
+	public void CheckEquippedSeed()
+	{
+		if (equippedSeed >= seeds.Count)
+		{
+			equippedSeed = 0;
+		}
+	}
+
+	public PlantData GetPlant()
+	{
+		if (seeds.Count == 0)
+		{
+			return null;
+		}
+		else
+		{
+			PlantData plant = seeds[equippedSeed].seed.Plant;
+			RemoveItem(seeds[equippedSeed].seed, 1);
+			return plant;
+		}
+		
+	}
+
+	public bool IsInventoryEmpty()
+	{
+		if (seeds.Count == 0)
+		{
+			return true;
+		}
+		else return false;
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -24,10 +24,15 @@ public class PlayerController : MonoBehaviour
 	[SerializeField] GameObject headbuttHitbox;
 
 	[Header("Planting")]
-	[SerializeField] List<PlantData> plants = new();
-	[SerializeField] private int equippedPlant = 0;
+	//[SerializeField] List<PlantData> plants = new();
+	private SeedInventory seedBag;
 	[SerializeField] LayerMask plotMask;
 	[SerializeField] LayerMask plantMask;
+
+	private void Awake()
+	{
+		seedBag = GetComponent<SeedInventory>();
+	}
 	private void Start()
 	{
 		InputManager.OnMove.AddListener(Move);
@@ -35,8 +40,8 @@ public class PlayerController : MonoBehaviour
 		InputManager.Headbutt.AddListener(Headbutt);
 		InputManager.TilPlant.AddListener(TilPlant);
 		InputManager.WaterCan.AddListener(WaterPlant);
-		InputManager.Next.AddListener(NextPlant);
-		InputManager.Previous.AddListener(PreviousPlant);
+		InputManager.Next.AddListener(seedBag.NextSeed);
+		InputManager.Previous.AddListener(seedBag.PreviousSeed);
 		_rb = GetComponent<Rigidbody2D>();
 	}
 	private void OnDestroy()
@@ -46,8 +51,8 @@ public class PlayerController : MonoBehaviour
 		InputManager.Headbutt?.RemoveListener(Headbutt);
 		InputManager.TilPlant?.RemoveListener(TilPlant);
 		InputManager.WaterCan?.RemoveListener(WaterPlant); 
-		InputManager.Next?.RemoveListener(NextPlant);
-		InputManager.Previous?.RemoveListener(PreviousPlant);
+		InputManager.Next?.RemoveListener(seedBag.NextSeed);
+		InputManager.Previous?.RemoveListener(seedBag.PreviousSeed);
 	}
 
 	private void FixedUpdate()
@@ -126,7 +131,11 @@ public class PlayerController : MonoBehaviour
 				if (plotHit.gameObject.GetComponent<TilePlot>().IsAlreadyTilled())
 				{
 					Debug.Log("Plant");
-					plotHit.gameObject.GetComponent<TilePlot>().PlaceNewPlant(plants[equippedPlant]);
+					if (!seedBag.IsInventoryEmpty())
+					{
+						plotHit.gameObject.GetComponent<TilePlot>().PlaceNewPlant(seedBag.GetPlant());
+					}
+					
 				}
 				else
 				{
@@ -145,23 +154,5 @@ public class PlayerController : MonoBehaviour
 		{
 			plantHit.gameObject.GetComponent<IPlant>().WaterPlant();
 		}
-	}
-
-	private void NextPlant()
-	{
-		if (equippedPlant + 1 == plants.Count)
-		{
-			equippedPlant = 0;
-		}
-		else equippedPlant += 1;
-	}
-
-	private void PreviousPlant()
-	{
-		if (equippedPlant == 0)
-		{
-			equippedPlant = plants.Count - 1;
-		}
-		else equippedPlant -= 1;
 	}
 }
