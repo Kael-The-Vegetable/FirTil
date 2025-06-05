35c6c6d12be26ac383f1eae53da50d1504878525
DylanDoty <127159556+DylanDoty@users.noreply.github.com>
2025-06-05 15:52:12 -0600

    Created a charge method for Moose

--- Assets/Scripts/Enemies/EnemyScripts/MooseScript.cs
+++ Assets/Scripts/Enemies/EnemyScripts/MooseScript.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 public class MooseScript : EnemyMain
 {
 	[SerializeField] Animator animator;
-	[SerializeField] float slamAttackSpeed;
-	[SerializeField] float timeStamp;
+	[SerializeField] float slamAttackSpeed, slamAttackTimeStamp;
+	[SerializeField] float chargeTimeStamp, chargeDuration ,chargeCoolDown;
+	[SerializeField] bool charging;
 
 	// Start is called once before the first execution of Update after the MonoBehaviour is created
 	void Start()
@@ -15,9 +16,14 @@ public class MooseScript : EnemyMain
 	// Update is called once per frame
 	void Update()
 	{
-		if (Time.time >= timeStamp + slamAttackSpeed)
+		if (!charging && Time.time >= chargeTimeStamp + chargeCoolDown)
 		{
-			timeStamp = Time.time;
+			Charge();
+		}
+
+		if (!charging && Time.time >= slamAttackTimeStamp + slamAttackSpeed)
+		{
+			slamAttackTimeStamp = Time.time;
 			animator.SetBool("Attack", true);
 			Invoke(nameof(StopSlam), 1.5f);
 		}
@@ -26,5 +32,17 @@ public class MooseScript : EnemyMain
 	private void StopSlam()
 	{
 		animator.SetBool("Attack", false);
+	}
+	private void Charge()
+	{
+		charging = true;
+		movespeed *= 2;
+		Invoke(nameof(StopCharging), chargeDuration);
+	}
+	private void StopCharging()
+	{
+		charging = false;
+		movespeed /= 2;
+		chargeTimeStamp = Time.time;
 	}
 }
