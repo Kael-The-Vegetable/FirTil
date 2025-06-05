878fdd388c8f20d6b61d4b098e39a29770582094
Kael-The-Vegetable <kkostiuk3@nait.ca>
2025-06-04 19:03:19 -0600

    Game Manager

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : PersistentSingleton<GameManager>
+{
+	protected override void Initialize()
+	{
+		SceneManager.activeSceneChanged += ActiveSceneChange;
+	}
+
+	public void LoadScene(string sceneName, bool onlyScene = false, bool setActive = false)
+	{
+		SceneManager.LoadScene(sceneName, onlyScene ? LoadSceneMode.Single : LoadSceneMode.Additive);
+		if (!onlyScene && setActive) SceneManager.SetActiveScene(SceneManager.GetSceneByName(sceneName));
+	}
+
+	public async void LoadSceneAsync(string sceneName, bool onlyScene = false, bool setActive = false)
+	{
+		await SceneManager.LoadSceneAsync(sceneName, onlyScene ? LoadSceneMode.Single : LoadSceneMode.Additive);
+		if (!onlyScene && setActive) SceneManager.SetActiveScene(SceneManager.GetSceneByName(sceneName));
+	}
+
+	private void ActiveSceneChange(Scene old, Scene nu)
+	{
+		switch (nu.name)
+		{
+			case "MainMenu":
+				break;
+			case "GameScene":
+				break;
+			default:
+				Debug.LogError(nu.name + " | Unrecognized Scene Name to be Active Scene");
+				break;
+		}
+	}
+}
